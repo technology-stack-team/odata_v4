3210210a2c63abd92e13e48ef47f6e3f53fee585
rj3197 <golu.ajain@gmail.com>
2022-11-29 15:10:31 +0530

    added support for timeofday , added tags property (collection of strings) to trip

--- trippin/src/main/java/org/example/trippin/model/Trip.java
+++ trippin/src/main/java/org/example/trippin/model/Trip.java
@@ -2,15 +2,21 @@ package org.example.trippin.model;
 
 import lombok.Data;
 
+import javax.persistence.CollectionTable;
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity(name = "Trip")
 @Table(schema = "\"Trippin\"", name = "\"Trip\"")
@@ -38,6 +44,12 @@ public class Trip {
   @Column(name = "\"Description\"")
   private String description;
 
+  @Column(name = "\"Tag\"")
+  @ElementCollection(fetch = FetchType.LAZY)
+  @CollectionTable(schema = "\"Trippin\"", name = "\"TripTag\"",
+          joinColumns = @JoinColumn(name = "\"TripId\""))
+  private List<String> tags = new ArrayList<>();
+
   @Column(name = "\"StartsAt\"")
   @Temporal(TemporalType.TIMESTAMP)
   private Date startsAt;
@@ -45,4 +57,13 @@ public class Trip {
   @Column(name = "\"EndsAt\"")
   @Temporal(TemporalType.TIMESTAMP)
   private Date endsAt;
+
+  @Column(name = "\"StartTime\"")
+  @Temporal(TemporalType.TIME)
+  private Date startTime;
+
+  @Column(name = "\"EndTime\"")
+  @Temporal(TemporalType.TIME)
+  private Date endTime;
+
 }
