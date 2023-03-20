9b4d0fcf859aeb7a162d81e6190f2fe654b8b4cc
Ganapathi Ram <ram@replicacia.com>
2023-03-20 18:31:27 +0530

    added ManyToMany annotation import statement

--- trippin/src/main/java/com/refapps/trippin/model/Trip.java
+++ trippin/src/main/java/com/refapps/trippin/model/Trip.java
@@ -16,6 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
